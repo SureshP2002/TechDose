6b11957a25a205206d47a9be601cce09914181e0
SureshP2002 <sureshpanneer2002@gmail.com>
2022-09-11 16:43:02 +0530

    ASSIGNMENTS

---
+++ BEGINNERS ASSIGNMENT/AreaOfTriangle.java
@@ -0,0 +1,12 @@
+package BeginnersAssignment;
+import java.util.*;
+
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+    }
+    
+    
+}

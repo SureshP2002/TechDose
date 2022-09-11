0ce97af0bcc7df7939e43d2b8857650566a00f7f
SureshP2002 <sureshpanneer2002@gmail.com>
2022-09-11 18:35:09 +0530

    ASSIGNMENT DONE

---
+++ BEGINNERSASSIGNMENT/areaOfCircle.java
@@ -0,0 +1,12 @@
+package BEGINNERSASSIGNMENT;
+import java.util.*;
+
+public class areaOfCircle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int radius=sc.nextInt();//getting radius;
+        System.out.print((3.14)*radius*radius);//formula (pi*r*r);
+        
+    }
+    
+}

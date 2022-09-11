4d1bbfac6126b3df1546408eee525c3d0c8202fe
SureshP2002 <sureshpanneer2002@gmail.com>
2022-09-11 21:07:09 +0530

    ASSIGNMENT DONE

---
+++ BEGINNERSASSIGNMENT/ADDintegerTOdecimalpoint.java
@@ -0,0 +1,12 @@
+package BEGINNERSASSIGNMENT;
+import java.util.*;
+
+public class ADDintegerTOdecimalpoint {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+       double d=sc.nextDouble();
+       System.out.println(n+d);
+    }
+    
+}

61ceabb24afd54dc86a54efa9c0073fd631c53d4
SureshP2002 <104820512+SureshP2002@users.noreply.github.com>
2022-09-23 22:57:52 +0530

    Create MaximumSubArraySum.java

---
+++ Array/MaximumSubArraySum.java
@@ -0,0 +1,20 @@
+//leetcode-53;
+//TC:O(n);
+//SC:O(1);
+//kadanes's Algorihtm
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=nums[0];
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+         
+              sum+=nums[i];
+              if(sum>max)max=sum;
+              if(sum<0)sum=0;
+          
+        }
+        return max;
+        
+    }
+}

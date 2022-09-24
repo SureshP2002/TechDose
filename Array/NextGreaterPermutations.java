e5f5ca4d4ab097c764c17c7683768dc60d79bacb
SureshP2002 <104820512+SureshP2002@users.noreply.github.com>
2022-09-24 21:04:55 +0530

    Update NextGreaterPermutations.java

--- Array/NextGreaterPermutations.java
+++ Array/NextGreaterPermutations.java
@@ -4,6 +4,7 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int index=-1;
+        //chechking for peak;
         for(int i=1;i<nums.length;i++)
         {
             if(nums[i-1]<nums[i])
@@ -11,12 +12,14 @@ class Solution {
                 index=i;
             }
         }
+        //if no peak then it is sorted;
         if(index==-1)
         {
             Arrays.sort(nums);
             return;
         }
         int swapIndex=index;
+        //finding the element which is greater then second peak and less then peak;
         for(int i=index;i<nums.length;i++)
         {
             if(nums[i]>nums[index-1]&&nums[i]<nums[swapIndex])
@@ -24,9 +27,11 @@ class Solution {
                 swapIndex=i;
             }
         }
+        //swaping element with second peak;
         nums[index-1]=nums[index-1]^nums[swapIndex];
         nums[swapIndex]=nums[index-1]^nums[swapIndex];
         nums[index-1]=nums[index-1]^nums[swapIndex];
+        //after sorting the elements from peak;
         Arrays.sort(nums,index,nums.length);
         
     }

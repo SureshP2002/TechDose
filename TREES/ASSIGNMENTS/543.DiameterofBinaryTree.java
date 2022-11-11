79465c20f3ff6d7f68ecb30bf9ded3b301172b98
SureshP2002 <104820512+SureshP2002@users.noreply.github.com>
2022-11-11 20:43:22 +0530

    ASSIGNMENT DONE

---
+++ TREES/ASSIGNMENTS/543.DiameterofBinaryTree.java
@@ -0,0 +1,34 @@
+//leetcode-543
+//approach-recursion
+//TC-O(N)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count=0;//initializing
+    public int diameterOfBinaryTree(TreeNode root) {
+        Count(root);//function call
+        return count;
+    }
+    public int  Count(TreeNode root)
+    {
+        if(root==null)return 0;//if root is null return 0;
+        int left=Count(root.left);//left call
+        int right=Count(root.right);//right call
+        count=Math.max(count,left+right);//finding count value at every subtree
+        return 1+Math.max(left,right);//finally returning max of left and right +1;
+    }
+}

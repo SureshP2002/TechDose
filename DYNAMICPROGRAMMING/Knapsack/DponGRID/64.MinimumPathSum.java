da3e1f3ef844ff15bde30691c874c56b03fc883d
SureshP2002 <104820512+SureshP2002@users.noreply.github.com>
2023-01-11 19:59:36 +0530

    ASSIGNMENT DONE

---
+++ DYNAMICPROGRAMMING/Knapsack/DponGRID/64.MinimumPathSum.java
@@ -0,0 +1,20 @@
+//LC-64
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int r=grid.length;
+        int c=grid[0].length;
+        int dp[][]=new int[r][c];
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(i==0&&j==0)dp[i][j]=grid[0][0];
+                else if(i==0)dp[i][j]=dp[i][j-1]+grid[i][j];
+                else if(j==0)dp[i][j]=dp[i-1][j]+grid[i][j];
+                else dp[i][j]=grid[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[r-1][c-1];
+    }
+}
